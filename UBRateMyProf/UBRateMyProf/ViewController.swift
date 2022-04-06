a321b076ec1322106aaea31b1e2111a6591981df
William Krasnov <willkoman@gmail.com>
2022-04-06 18:06:34 -0400

    App Base

---
+++ UBRateMyProf/UBRateMyProf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UBRateMyProf
+//
+//  Created by William Krasnov on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
