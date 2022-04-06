f77649301c73b39603b7a9356f3e7718410339a8
William Krasnov <willkoman@gmail.com>
2022-04-06 18:10:43 -0400

    Pods

---
+++ UBRateMyProf/Pods/Target Support Files/Pods-UBRateMyProf-UBRateMyProfUITests/Pods-UBRateMyProf-UBRateMyProfUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UBRateMyProf_UBRateMyProfUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UBRateMyProf_UBRateMyProfUITests
+@end
---
+++ UBRateMyProf/Pods/Target Support Files/Pods-UBRateMyProfTests/Pods-UBRateMyProfTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UBRateMyProfTests : NSObject
+@end
+@implementation PodsDummy_Pods_UBRateMyProfTests
+@end
