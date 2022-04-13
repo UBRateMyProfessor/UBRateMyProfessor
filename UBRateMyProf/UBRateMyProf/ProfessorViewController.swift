42087ce41405fb0e09c20dbd998e01d7d8b244f2
achen236 <achen236@buffalo.edu>
2022-04-12 20:15:26 -0400

    Ratings tableview

---
+++ UBRateMyProf/UBRateMyProf/ProfessorViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProfessorViewController.swift
+//  UBRateMyProf
+//
+//  Created by Andy Chen on 4/12/22.
+//
+
+import UIKit
+
+class ProfessorViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = 150
+    
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RatingCell") as! RatingCell
+        return cell
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UBRateMyProf/UBRateMyProf/RatingCell.swift
@@ -0,0 +1,23 @@
+//
+//  RatingCell.swift
+//  UBRateMyProf
+//
+//  Created by Andy Chen on 4/12/22.
+//
+
+import UIKit
+
+class RatingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
