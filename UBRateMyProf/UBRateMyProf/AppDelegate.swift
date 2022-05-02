6d58886c05fdf6c2fa89b2a17f7dd42187476687
achen236 <achen236@buffalo.edu>
2022-05-02 18:12:53 -0400

    Professor's page and reviews functional

--- UBRateMyProf/UBRateMyProf/AppDelegate.swift
+++ UBRateMyProf/UBRateMyProf/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "714Z0L2gOUsO3BLGIRNzN6idrqYsPdQlRCtLwliY" // <- UPDATE
+                    $0.clientKey = "Wm9kA8OpuLWK8gLUQyanJThwdLYUMe6mVH08e8Mo" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
--- UBRateMyProf/UBRateMyProf/ProfessorViewController.swift
+++ UBRateMyProf/UBRateMyProf/ProfessorViewController.swift
@@ -16,6 +16,8 @@ class ProfessorViewController: UIViewController, UITableViewDataSource, UITableV
     @IBOutlet weak var tableView: UITableView!
     
     let nameSearch : String =  "Hartloff"
+    var professorInfo : PFObject!
+    var professorReviews = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,28 +29,70 @@ class ProfessorViewController: UIViewController, UITableViewDataSource, UITableV
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool){
         super.viewDidAppear(animated)
+        // Get professor info and reviews
+        getProfessor()
+    }
+    
+    func getProfessor(){
         // (Read/GET) GET “name” in list of professors
-        let query = PFQuery(className:"Professors")
-        query.whereKey("last_name", matchesText: nameSearch)
-        query.order(byDescending: "Name")
-        query.findObjectsInBackground { (professors: [PFObject]?, error: Error?) in
+        let queryProfessor = PFQuery(className:"Professors")
+        queryProfessor.whereKey("last_name", matchesText: nameSearch)
+        queryProfessor.order(byDescending: "Name")
+        queryProfessor.findObjectsInBackground { [self] (professors: [PFObject]?, error: Error?) in
            if let error = error {
               print(error.localizedDescription)
-           } else if let professors=professors {
-              print("Successfully retrieved \(professors.count) professors.")
-              // TODO: Do something with professor list...
+           }else if let professors=professors {
+               print("Successfully retrieved \(professors.count) professors.")
+               // Save professor info
+               self.professorInfo = professors[0]
+               // Update with professor's details
+               self.professorName.text = "\(self.professorInfo["first_name"] ?? "firstName") \(self.professorInfo["last_name"] ?? "lastName")"
+               self.professorDepartment.text = "\(self.professorInfo["department"] ?? "department")"
+               self.professorRating.text = "\(self.professorInfo["overall_rating"] ?? "NA")"
+               getReviews()
+           }
+        }
+    }
+    
+    func getReviews(){
+        // Get professor reviews
+        // (Read/GET) GET “name” in list of professors
+        let queryReviews = PFQuery(className:"Ratings")
+        queryReviews.whereKey("ratemyprof_id", equalTo: self.professorInfo["ratemyprof_id"] ?? 0)
+        queryReviews.order(byDescending: "createdAt")
+        queryReviews.findObjectsInBackground { (reviews: [PFObject]?, error: Error?) in
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let reviews = reviews {
+               print("Successfully retrieved \(reviews.count) reviews.")
+               // Save reviews
+               self.professorReviews = reviews
+               self.tableView.reloadData()
            }
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return professorReviews.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell") as! ReviewCell
+        
+        let review = self.professorReviews[indexPath.row]
+        let title = review["rDate"] as! String
+        let course = review["rClass"] as! String
+        let content = review["rComments"] as! String
+        let rating = String(review["rOverall"] as! Float)
+        
+        cell.reviewTitle.text = title
+        cell.reviewCourse.text = course
+        cell.reviewContent.text = content
+        cell.reviewRating.text = rating
+        
+        
         return cell
     }
     
